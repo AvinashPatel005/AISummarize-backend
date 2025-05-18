ebbd671e0ffbf945e8fdaf82764e848bf79514e4
Avinash <awinash123patel@gmail.com>
2025-05-18 23:55:01 +0530

    -

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth');
+
 const {
   getNotes,
   addNote,
@@ -8,8 +8,6 @@ const {
   deleteNote,
   summarizeNote
 } = require('../controllers/notesController');
-
-router.use(auth);
 
 router.get('/', getNotes);
 router.post('/', addNote);
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-
+const auth = require('../middleware/auth');
 dotenv.config();
 
 const app = express();
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/notes', require('./routes/notes'));
+app.use('/api/notes',auth, require('./routes/notes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
